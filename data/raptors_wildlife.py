50c26f195e54f70736da25fa1bbb0d7a67f693f7
Amee Assad <aa3628@columbia.edu>
2024-10-27 10:14:37 -0400

    debug

--- data/raptors_wildlife.py
+++ data/raptors_wildlife.py
@@ -600,6 +600,8 @@ class WildlifeReidDataModule(pl.LightningDataModule):
             return None
         if not isinstance(df[segmentation_col], (list, str)):
             print("No segmentation data found.: Not a list or string")
+            print(f"type of segmentation column: {type(df[segmentation_col])}")
+            print(f"segmentation column: {df[segmentation_col]}")
             return None
 
         print("TYPES OF VALUES IN SEGMENTATION COLUMN:")
