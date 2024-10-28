c2009c53a667826b4d7a7515a2e97e30c325569b
Amee Assad <aa3628@columbia.edu>
2024-10-28 18:03:59 -0400

    debug

--- data/raptors_wildlife.py
+++ data/raptors_wildlife.py
@@ -485,9 +485,9 @@ class WildlifeReidDataModule(pl.LightningDataModule):
             df_query = add_segmentations(df_query, self.data_dir, cache_path=self.cache_path, only_cache=self.only_cache)
             df_gallery = add_segmentations(df_gallery, self.data_dir, cache_path=self.cache_path, only_cache=self.only_cache)
 
-            # print(f"df_train: {len(df_train)}, columns: {df_train.columns} and values: {df_train.iloc[0]}")
-            # print(f"df_query: {len(df_query)}, columns: {df_query.columns} and values: {df_query.iloc[0]}")
-            # print(f"df_gallery: {len(df_gallery)}, columns: {df_gallery.columns} and values: {df_gallery.iloc[0]}")
+            print(f"df_train: {len(df_train)}, columns: {df_train.columns} and values: {df_train.iloc[0]}")
+            print(f"df_query: {len(df_query)}, columns: {df_query.columns} and values: {df_query.iloc[0]}")
+            print(f"df_gallery: {len(df_gallery)}, columns: {df_gallery.columns} and values: {df_gallery.iloc[0]}")
 
             df_train = self.clean_segmentation(df_train)
             df_query = self.clean_segmentation(df_query)
