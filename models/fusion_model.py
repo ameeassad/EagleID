07ed5b3ca24d1d53668f10d0095cef99cae5a456
Amee Assad <aa3628@columbia.edu>
2025-03-24 15:07:25 -0400

    fix fusion multiply by kp groups

--- models/fusion_model.py
+++ models/fusion_model.py
@@ -83,7 +83,10 @@ class FusionModel(pl.LightningModule):
 
         # Embedding layer
         if self.preprocess_lvl >= 3:
-            total_feature_dim = self.backbone.feature_info[-1]['num_chs'] + self.backbone_kp.feature_info[-1]['num_chs']
+            total_feature_dim = (
+                self.backbone.feature_info[-1]['num_chs'] + 
+                (self.num_kp_groups * self.backbone_kp.feature_info[-1]['num_chs'])  # Multiply by num_kp_groups
+            )
         else:
             total_feature_dim = self.backbone.feature_info[-1]['num_chs']
         self.embedding = nn.Linear(total_feature_dim, self.embedding_size)
