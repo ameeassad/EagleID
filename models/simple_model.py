28ce3b6c71620bdf11fcb968e3cd3e31541ffec6
Amee Assad <aa3628@columbia.edu>
2025-06-30 23:00:01 +0200

    agle classifier training

--- models/simple_model.py
+++ models/simple_model.py
@@ -11,7 +11,7 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 from PIL import Image
 from pytorch_lightning import LightningDataModule, LightningModule, Trainer
-from torchmetrics import Accuracy
+from torchmetrics import Accuracy, Precision, Recall, F1Score
 
 from utils.triplet_loss_utils import TripletLoss
 from utils.optimizer import get_optimizer, get_lr_scheduler_config
@@ -28,19 +28,27 @@ class SimpleModel(LightningModule):
         self.save_hyperparameters()
         self.config = config
 
-        self.model = timm.create_model(model_name=config['model_name'], pretrained=pretrained, num_classes=num_classes)
+        self.model = timm.create_model(model_name=config['backbone_name'], pretrained=pretrained, num_classes=num_classes)
 
         # if skeleton, accept 4 channels instead of 3
-        if config['preprocess_lvl']==3 and config['model_name'].startswith('resnet'):
+        if config['preprocess_lvl']==3 and config['backbone_name'].startswith('resnet'):
             self.model.conv1 = nn.Conv2d(4, self.model.conv1.out_channels, kernel_size=self.model.conv1.kernel_size,
                                          stride=self.model.conv1.stride, padding=self.model.conv1.padding, bias=False)
         
-        self.train_loss = nn.CrossEntropyLoss()
+        label_smoothing = config.get('classifier', {}).get('label_smoothing', 0.0)
+        self.train_loss = nn.CrossEntropyLoss(label_smoothing=label_smoothing)
+        self.val_loss = nn.CrossEntropyLoss(label_smoothing=label_smoothing)
         self.train_acc = Accuracy(task='multiclass', num_classes=num_classes)
-        self.val_loss = nn.CrossEntropyLoss()
         self.val_acc = Accuracy(task='multiclass', num_classes=num_classes)
+        self.val_precision = Precision(task='multiclass', num_classes=num_classes, average='macro')
+        self.val_recall = Recall(task='multiclass', num_classes=num_classes, average='macro')
+        self.val_f1 = F1Score(task='multiclass', num_classes=num_classes, average='macro')
+        self.val_top2 = Accuracy(task='multiclass', num_classes=num_classes, top_k=2)
         self.gradient = None
         self.outdir = self.config['outdir']
+
+        # self.train_loss = nn.CrossEntropyLoss()
+        # self.val_loss = nn.CrossEntropyLoss()
     
     def activations_hook(self, grad):
         self.gradient = grad
@@ -79,15 +87,18 @@ class SimpleModel(LightningModule):
         x, target = batch
         out = self(x)
         _, pred = out.max(1)
-
         loss = self.val_loss(out, target)
         acc = self.val_acc(pred, target)
-        # self.log_dict({'val/loss': loss, 'val/acc': acc})
-
-        # Log validation loss and accuracy
+        precision = self.val_precision(pred, target)
+        recall = self.val_recall(pred, target)
+        f1 = self.val_f1(pred, target)
+        top2 = self.val_top2(out, target)
         self.log('val/loss', loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
         self.log('val/acc', acc, on_step=False, on_epoch=True, prog_bar=True, logger=True)
-
+        self.log('val/precision', precision, on_step=False, on_epoch=True, prog_bar=False, logger=True)
+        self.log('val/recall', recall, on_step=False, on_epoch=True, prog_bar=False, logger=True)
+        self.log('val/f1', f1, on_step=False, on_epoch=True, prog_bar=False, logger=True)
+        self.log('val/top2_acc', top2, on_step=False, on_epoch=True, prog_bar=False, logger=True)
         return loss
 
     def test_step(self, batch, batch_idx):
